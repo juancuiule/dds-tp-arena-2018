243a6899bac5827915ef44bd7b54d57ab570930d
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-07-09 16:47:04 -0300

    estructura basica

---
+++ src/main/model/Estudiante.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Estudiante {
+
+	String nombre;
+	String legajo;
+	String contraseña;
+	String usuarioGithub;
+
+	public Estudiante(String nombre, String legajo, String contraseña, String usuarioGithub) {
+		this.nombre = nombre;
+		this.legajo = legajo;
+		this.contraseña = contraseña;
+		this.usuarioGithub = usuarioGithub;
+	}
+
+}
