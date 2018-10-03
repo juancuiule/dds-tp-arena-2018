5a5761f6d02bd5be039b4f99015dc10dc00bfa43
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2018-10-03 12:43:19 -0300

    test assignments approved

--- src/test/java/modelTest/AsignacionesTest.java
+++ src/test/java/modelTest/AsignacionesTest.java
@@ -1,74 +1,52 @@
-//package modelTest;
-//
-//import static org.junit.Assert.*;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import com.sun.jersey.api.client.ClientResponse;
-//
-//import json.JSONParser;
-//import model.Assignment;
-//import model.Estudiante;
-//import model.Grade;
-//import model.NotaConceptual;
-//import model.NotaNumerica;
-//import model.StudentController;
-//
-//public class AsignacionesTest {
-//	Estudiante juan;
-//	List<Grade> notasParcial;
-//	Assignment parcialJuan;
-//
-//	List<Grade> notasTP;
-//	Assignment tpJuan;
-//
-//	@Before
-//	public void fixture() {
-////		parcialDDS = new Tarea("1° Parcial - Diseño");
-////		tpGDD = new Tarea("Trabaoj Practico - Gestión");
-//
-//		juan = new Estudiante("1589386", "Juan", "Cuiule", "juancuiule", "1234abcd");
-//
-//		notasParcial = new ArrayList<Grade>(Arrays.asList(new NotaNumerica(6.0)));
-//		parcialJuan = new Asignacion("Primer", notasParcial);
-//
-//		notasTP = new ArrayList<Nota>(Arrays.asList(new NotaConceptual("R")));
-//		tpJuan = new Asignacion(juan, tpGDD, notasTP);
-//	}
-//
-//	@Test
-//	public void elParcialEstaAprobado() {
-//		assertTrue(parcialJuan.aprobada());
-//	}
-//
-//	@Test
-//	public void elTpNoEstaAprobado() {
-//		assertFalse(tpJuan.aprobada());
-//	}
-//
-//	@Test
-//	public void elTpPasaAEstarAprobado() {
-//		tpJuan.agregarNota(new NotaConceptual("B-"));
-//		assertTrue(tpJuan.aprobada());
-//	}
-//
-//	@Test
-//	public void requestStudent() {
-//		ClientResponse response = new StudentController().getStudent();
-//		//Estudiante x = JsonParser.objetoDesdeString(response.getEntity(String.class), Estudiante.class);
-//		assertEquals(200, response.getStatus());
-//	}
-//
-//	@Test
-//	public void requestAssigments() {
-//		ClientResponse response = new StudentController().getAssignments();
-//		String x = JSONParser.parseAssignments(response.getEntity(String.class));
-//		assertEquals(200, response.getStatus());
-//		System.out.println(x);
-//	}
-//}
+package modelTest;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Assignment;
+import model.Student;
+import model.Grade;
+import model.ConceptualGrade;
+import model.NumericGrade;
+
+public class AsignacionesTest {
+	Student juan;
+	List<Grade> notasParcial;
+	Assignment parcialJuan;
+
+	List<Grade> notasTP;
+	Assignment tpJuan;
+
+	@Before
+	public void fixture() {
+		juan = new Student("Juan", "Cuiule", "1589386", "juancuiule", "1234abcd");
+
+		notasParcial = new ArrayList<Grade>(Arrays.asList(new NumericGrade(6.0)));
+		parcialJuan = new Assignment("Primer Parcial", "parcial primer cuatrimestre", notasParcial);
+
+		notasTP = new ArrayList<Grade>(Arrays.asList(new ConceptualGrade("R")));
+		tpJuan = new Assignment("TP GDD", "trabajo practico segundo cuatrimestre 2018", notasTP);
+	}
+
+	@Test
+	public void elParcialEstaAprobado() {
+		assertTrue(parcialJuan.approved());
+	}
+
+	@Test
+	public void elTpNoEstaAprobado() {
+		assertFalse(tpJuan.approved());
+	}
+
+	@Test
+	public void elTpPasaAEstarAprobado() {
+		tpJuan.agregarNota(new ConceptualGrade("B-"));
+		assertTrue(tpJuan.approved());
+	}
+}
